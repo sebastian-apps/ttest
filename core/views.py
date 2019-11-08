a0638d007f1b8e9e810344e088f4004d9af781c1
Sebastian Sessarego <56331348+sebastian-apps@users.noreply.github.com>
2019-11-07 22:13:11 -0700

    Update views.py

--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ import json
 import numpy as np
 import math
 import statistics
-
+from statsmodels.stats.power import  tt_ind_solve_power
 
 
 DEC = 4  # Round values to a constant number of decimal places.
@@ -35,26 +35,22 @@ class Data:
         self.step_size = 0;
 
 
-    def create_t_dist(self, min, max):
+    def create_t_dist(self, min, max, ncp):
         """ Create x,y coordinates of t distribution for a given range. """
-        self.step_size = (max - min) / 50   # 50 is an arbitrary number
-        x_list = [round(min + self.step_size*i, 6) for i in range(0, 50)]
+        self.step_size = (max - min) / 200   # 200 is an arbitrary number
+        t_list = [round(min + self.step_size*i, 6) for i in range(0, 200)]
 
         try:
             coords = []
-            for x in x_list:
-                t = (x-self.mean)/self.sd
+            for t in t_list:
                 y = round((math.gamma((self.df+1)/2)/(math.sqrt(self.df*math.pi)*math.gamma(self.df/2))) * (1+((t**2)/self.df))**(-(self.df+1)/2),DEC)
-                coords.append({'x': x, 'y': y})
+                coords.append({'x': t + ncp, 'y': y})
             self.dist = coords
 
         except Exception as e:
             print(str(e))
             self.dist = []
 
-
-    def convert_t_to_x(self, t):
-        return round(self.mean + (abs(t) * self.sd), DEC)
 
 
     def __str__(self):
@@ -87,7 +83,7 @@ def ttest(request):
         form = DatasetsForm()
         # Default datasets
         dataset1 = [87, 101, 64, 86, 87, 82, 70]
-        dataset2 = [100, 124, 93, 114, 123, 130, 136, 120, 107, 69, 120]
+        dataset2 = [100, 124, 93, 114, 123, 130, 136]
         form.fields['dataset1'].initial = prep_for_form(dataset1)
         form.fields['dataset2'].initial = prep_for_form(dataset2)
 
@@ -111,6 +107,16 @@ def ttest(request):
 
         data1 = Data(dataset1)
         data2 = Data(dataset2)
+
+        # data1.mean = 5
+        # data1.sd = 15
+        # data1.n = 42
+        # data1.df = data1.n - 1
+        # data2.mean = 15
+        # data2.sd = 17
+        # data2.n = 42
+        # data2.df = data2.n - 1
+
         # Sample with largest mean will be group 2
         if data1.mean > data2.mean:
             data1, data2 = data2, data1
@@ -119,14 +125,15 @@ def ttest(request):
 
         # Get inferential statistics
         df = get_pooled_df(data1, data2) # degrees of freedom
+
         t_value, p_value = get_t_and_p_value(data1, data2, df)
-        t_value_x = data1.convert_t_to_x(t_value)
+        effect_size, ncp = get_effect_size_ncp(data1, data2)
 
         # Define chart's x-axis range.
         x_min, x_max = get_x_axis_min_max(data1, data2)
         # Get distribution x,y values for plotting.
-        data1.create_t_dist(x_min, x_max)
-        data2.create_t_dist(x_min, x_max)
+        data1.create_t_dist(x_min, x_max, 0)
+        data2.create_t_dist(x_min, x_max, ncp)
 
 
         # Configure the x-axis step size for a more visually appealing chart.
@@ -143,7 +150,10 @@ def ttest(request):
         # Define chart's y-axis range: 0 to y_max. Multiply by arbitrary value to provide headspace.
         y_max = 1.2 * get_y_axis_max(data1.dist, data2.dist)
 
-        test_results.update({"df" : df, "t_value" : t_value, "p_value" : p_value, "t_value_x" : t_value_x})
+        test_results.update({"df" : df, "p_value" : p_value, "t_value" : t_value,
+                             "effect_size": effect_size, "ncp" : ncp
+                             })
+
         test_results.update({"crit_t_init": st.norm.ppf(.95)})  # Arbitrary starting point (alpha=0.05), opposite is st.norm.cdf(1.64)
         axes.update({"x_min": x_min, "x_max": x_max, "y_max": y_max, "step_size": step_size})
 
@@ -178,13 +188,32 @@ def get_t_and_p_value(data1, data2, df):
     return round(t,DEC), round(p_value,DEC)
 
 
+def get_effect_size_ncp(data1, data2):
+    mean_difference = data1.mean - data2.mean
+    num = ((data1.n - 1)*(data1.sd**(2))) + ((data2.n - 1)*(data2.sd**(2)))
+    den = data1.n + data2.n - 2
+    sd = math.sqrt(num/den)  # pooled sd
+    es = abs(mean_difference / sd)
+    nobs_ratio = data1.n / data2.n
+
+    # error check front-side power calculation with backend power calculation
+    power = tt_ind_solve_power(effect_size=es, nobs1=data1.n, alpha=0.05, ratio=nobs_ratio, alternative='larger')
+    print("POWER ",power)
+
+    p = 1./ (1./data1.n  + 1./data2.n)  # from statsmodels library code
+    ncp = es*math.sqrt(p)
+    return round(es, DEC), ncp
+
+
+
+
 def get_x_axis_min_max(data1, data2):
-    # Each tail of each distribution will have at least 6 standard deviations represented visually.
+    # Each tail of each distribution will have at least 2 * sd/sqrt(n) represented visually.
     vals = []
-    vals.append(round(data1.mean - (6 * data1.sd), 6))
-    vals.append(round(data1.mean + (6 * data1.sd), 6))
-    vals.append(round(data2.mean - (6 * data2.sd), 6))
-    vals.append(round(data2.mean + (6 * data2.sd), 6))
+    vals.append(round(0 - (2 * data1.sd/(math.sqrt(data1.n))), 6))
+    vals.append(round(0 + (2 * data1.sd/(math.sqrt(data1.n))), 6))
+    vals.append(round(0 - (2 * data2.sd/(math.sqrt(data2.n))), 6))
+    vals.append(round(0 + (2 * data2.sd/(math.sqrt(data2.n))), 6))
     return min(vals), max(vals)
 
 
