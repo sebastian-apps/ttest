d0cb7ab76f436132f5278a55e19d4a8814a6a0de
sebastian-apps <>
2021-01-01 19:15:00 -0700

    ready for deployment

--- ttest/settings.py
+++ ttest/settings.py
@@ -18,11 +18,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # Read secret key from a file
-try:
-    with open('k.txt') as f:
-        SECRET_KEY = f.read().strip()
-except Exception:
-    SECRET_KEY = 'secret key'
+# try:
+#     with open('k.txt') as f:
+#         SECRET_KEY = f.read().strip()
+# except Exception:
+SECRET_KEY = 'secret key'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
